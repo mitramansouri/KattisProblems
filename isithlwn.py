ec4062c645bdda8c26bcc467ddf7594b7c4f585b
mitra mansouri <nirvananaseri7@gmail.com>
2021-04-11 12:43:44 +0430

    isItHoloween

---
+++ isithlwn.py
@@ -0,0 +1,7 @@
+def isItHoloween():
+    line_of_input = input()
+    if line_of_input == "OCT 31" or line_of_input == "DEC 25":
+        print("yup")
+    else:
+        print("nope")
+isItHoloween()
